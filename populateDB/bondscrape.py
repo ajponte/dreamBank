5a67082a5005caac1a601b5744b446e598ecdcbf
= <alanjponte@gmail.com>
2014-10-11 19:26:31 -0700

    adding full dir

--- populateDB/bondscrape.py
+++ populateDB/bondscrape.py
@@ -10,6 +10,7 @@ import urllib2
 import unidecode
 from unidecode import unidecode
 import re
+import json
 
 
 #yahoo finance bond rates for treasury, municipal, and corporate bonds
@@ -17,28 +18,33 @@ import re
 
 yahoofinancebondurl="http://finance.yahoo.com/bonds/composite_bond_rates"
 
+def getBondData():
+    br = webdriver.Firefox()
+    br.get(yahoofinancebondurl)
+    bondhtml = br.page_source
+    bondsoup = BeautifulSoup(bondhtml)
+    headers=['Maturity','Yield','Yesterday','Last Week','Last Month']
+    dictforbonds={}
+    dictforbondname={}
+    for x in bondsoup.find_all('table'):
+        try:
+            x.find('caption').text
+        except AttributeError:
+            continue
+        chalk = x.caption.text
+        if re.search(r'Bonds',str(x.caption.text))!=None:
+            charm = x.tbody.find_all('tr')
+            for b in charm:
+                for y,q in zip(b.find_all('td'),headers):
+                    if q in dictforbondname.keys():
+                        dictforbondname[q].append(str(y.text))
+                    else:
+                        dictforbondname[q]=[str(y.text)]
+            dictforbonds[chalk]=dictforbondname
+            dictforbondname={}
+    return dictforbonds
 
-br=webdriver.Firefox()
-br.get(yahoofinancebondurl)
-bondhtml=br.page_source
-bondsoup= BeautifulSoup(bondhtml)
-headers=['Maturity','Yield','Yesterday','Last Week','Last Month']
-dictforbonds={}
-dictforbondname={}
-for x in bondsoup.find_all('table'):
-    try:
-        x.find('caption').text
-    except AttributeError:
-        continue
-    chalk=x.caption.text
-    if re.search(r'Bonds',str(x.caption.text))!=None:
-        charm=x.tbody.find_all('tr')
-        for b in charm:
-            for y,q in zip(b.find_all('td'),headers):
-                if q in dictforbondname.keys():
-                    dictforbondname[q].append(str(y.text))
-                else:
-                    dictforbondname[q]=[str(y.text)]
-        dictforbonds[chalk]=dictforbondname
-        dictforbondname={}
+if __name__ == "__main__":
+    bondData = getBondData()
+    print bondData
         
