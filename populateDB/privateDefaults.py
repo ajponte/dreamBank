94821af06c2c6169c738aa0a0d86ebe2e914b5db
Alan Ponte <alanjponte@gmail.com>
2015-01-21 09:21:04 -0800

    Update privateDefaults.py

--- populateDB/privateDefaults.py
+++ populateDB/privateDefaults.py
@@ -1,5 +1,5 @@
 myDefaults = {
-		'PASSWORD' : "R@gnaros89",
-		'USERNAME' : "aponte@lbl.gov",
+		'PASSWORD' : "*********",
+		'USERNAME' : "*********",
 		'SECURITY_TOKEN' : "Y35ffR4oThGaDIt53s5vFuE8U"
 	}
